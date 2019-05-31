5e34484a88a42fabbc152d32945f3d306b4d4a23
marielosagui <00078913@uca.edu.sv>
2019-05-31 10:41:46 -0600

    fin

---
+++ app/src/main/java/com/petrlr14/mvvm/database/RoomDB.kt
@@ -0,0 +1,36 @@
+package com.petrlr14.mvvm.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.petrlr14.mvvm.database.daos.GitHubDAO
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+
+@Database(entities = [GitHubRepo::class], version = 1, exportSchema = false)
+public abstract class RoomDB : RoomDatabase() {
+
+    abstract fun repoDao():GitHubDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RoomDB? = null
+
+        fun getInstance(
+            context: Context
+        ): RoomDB {
+            if (INSTANCE != null) return  INSTANCE!!
+            else {
+                synchronized(this){
+                    INSTANCE = Room
+                        .databaseBuilder(context, RoomDB::class.java, "Repo_Database")
+                        .build()
+                    return INSTANCE!!
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/petrlr14/mvvm/database/repositories/GitHubRepoRepository.kt
@@ -0,0 +1,31 @@
+package com.petrlr14.mvvm.database.repositories
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.petrlr14.mvvm.database.daos.GitHubDAO
+import com.petrlr14.mvvm.database.entities.GitHubRepo
+import com.petrlr14.mvvm.service.retrofit.GitHubService
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+
+class GitHubRepoRepository (private val repoDao:GitHubDAO, private  val gitHubService: GitHubService){
+
+    @WorkerThread
+    suspend fun insert(repo:GitHubRepo){
+        repoDao.insert(repo)
+    }
+
+    fun getAll():LiveData<List<GitHubRepo>>{
+        return repoDao.getAllRepos()
+    }
+
+    @WorkerThread
+    suspend fun nuke(){
+        return repoDao.nukeTable()
+    }
+
+    fun retrieveReposAsync(user:String): Deferred<Response<List<GitHubRepo>>> {
+        return gitHubService.getRepos(user)
+    }
+
+}
